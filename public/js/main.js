2c5341ebe9ef3448cae760a85b543263310d344d
colints <ctsygiel@gmail.com>
2017-11-25 18:20:00 +0100

    canvas setup

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,43 @@
-let shape = document.querySelector('#shape');
-let ctx = shape.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(0,0,50,50);
+(function(){
+  //global variables
+  let win=window;
+  let dpi=win.devicePixelRatio;
+
+  const getContext = (canvas) => {
+    return canvas.getContext('2d')
+  }
+  const setAttribute = (attr,value,element) => {
+    element.setAttribute(attr, value)
+  }
+  const getBounds = (element) => {
+    return element.getBoundingCLientRect()
+  }
+  const forEach = (array, callback) => {
+    for(let i=0;i<array.length;i++){
+      callback(array[i],i);
+    }
+  }
+  const createCanvas = (width,height,userDPI) => {
+    if(typeof userDPI=="undefined") userDPI=dpi;
+    let canvas=doc.createElement('canvas');
+    setAttribute('width',width*userDPI,canvas);
+    setAttribute('height',height*userDPI,canvas);
+    return canvas;
+  }
+  const sizeToBounds = (bounds, dpi, element) => {
+    setAttribute('width',bounds.width*dpi,element);
+    setAttribute('height',bounds.height*dpi,element);
+  }
+
+  const mountains = () => {
+    let canvases=querySelectorAll('.Scene-mountains')
+    forEach(canvases, function(canvas) {
+      let bounds=getBounds(canvas)
+      sizeToBounds(bounds,dpi,canvas)
+      let middle ={
+        x:bounds.width/2,
+        y:bounds.height/2
+      }
+    })
+  }
+}())
