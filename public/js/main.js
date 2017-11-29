0d4c43c2b8ecb1141f8516837c79c3b89ccd8250
colints <ctsygiel@gmail.com>
2017-11-29 18:40:57 +0100

    updated canvas

--- public/js/main.js
+++ public/js/main.js
@@ -12,49 +12,50 @@
   let dpi=win.devicePixelRatio;
   let raf=requestAnimationFrame
 
-  const getContext = (canvas) => {
+ function getContext (canvas){
     return canvas.getContext('2d')
   }
-  const setAttribute = (attr,value,element) => {
+  function setAttribute(attr,value,element){
     element.setAttribute(attr, value)
   }
-  const setFillStyle = (fill,ctx) => {
+  function setFillStyle(fill,ctx) {
     ctx.fillStyle=fill;
   }
-  const getBounds = (element) => {
+  function getBounds(element){
     return element.getBoundingClientRect()
   }
-  const forEach = (array, callback) => {
+  function forEach(array, callback){
     for(let i=0;i<array.length;i++){
       callback(array[i],i);
     }
   }
-  const repeat = (times, callback) => {
+  function repeat(times, callback){
     for(var i=0;i<times;i++){
       callback(i);
     }
   }
-  const querySelectorAll = (selector) => {
+  function querySelectorAll(selector){
     let nodes=document.querySelectorAll(selector);
     return [].slice.call(nodes);
   }
-  const querySelector = (selector) => {
+  function querySelector(selector){
     return document.querySelector(selector);
   }
-  const createCanvas = (width,height,userDPI) => {
+  function createCanvas(width,height,userDPI) {
     if(typeof userDPI=="undefined") userDPI=dpi;
-    let canvas=doc.createElement('canvas');
+    let canvas=document.createElement('canvas');
     setAttribute('width',width*userDPI,canvas);
     setAttribute('height',height*userDPI,canvas);
     return canvas;
   }
-  const sizeToBounds = (bounds, dpi, element) => {
+  function sizeToBounds(bounds, dpi, element){
     setAttribute('width',bounds.width*dpi,element);
     setAttribute('height',bounds.height*dpi,element);
   }
 
-  const mountains = () => {
+  function mountains(){
     let canvases=querySelectorAll('.Scene-mountains')
+    let texture=null
     forEach(canvases, function(canvas) {
       let bounds=getBounds(canvas)
       sizeToBounds(bounds,dpi,canvas)
@@ -62,64 +63,76 @@
         x:bounds.width/2,
         y:bounds.height/2
       }
+      if(texture==null){
+        texture=createMountains();
+      }
+      let textureMountains=texture
+      canvas.style.opacity=0.9999;
+
+      function createMountains(){
+        let textureCanvas=createCanvas(bounds.width, bounds.height)
+        sizeToBounds(bounds, dpi, textureCanvas)
+        let ctx=getContext(textureCanvas)
+        let factor=20
+        if(win.innerWidth<700) factor=15
+        let cols=Math.round(bounds.width/factor)
+        let gridSize=bounds.width/cols
+        let mountains={
+          left:[
+            '......kkkkjjjkkjjkjkkjjkkkkkjjjjjj..kkjj',
+            '....kkkjjj..kkkkjkkjjjj.......kkkkjjjjj',
+            '...kkjj...kkkkjjj..kkkjkjjjj.....kkkkjjkjjj',
+          ],
+          right:[
+            '......kkkkjjjkkjjjkkkjjkkkkkjjjjjkjj..kkkjjj',
+            '....kkkjjjkkkkjjjkkjj.......kkkkjjjjj',
+            '...kkjj.kkkkjjkkjjjjkkkkjkjjjj...kkkkjkjjjj',
+          ]
+        }
+        function drawLayer(layer, directionX, directionY) {
+          ctx.beginPath()
+          let pos=0
+          let started=false
+          repeat(layer.length, (i) => {
+            let dir=layer.charAt(i)
+            pos+=dir=='.'?0:(dir=='j'?-1:1)
+            if(pos!=0 && !started){
+              started=true
+              ctx.moveTo((middle.x+((i-1)*gridSize*directionX))*dpi,middle.y*dpi);
+            }
+            ctx.lineTo(
+              (middle.x+(i*gridSize)*directionX)*dpi,
+              (middle.y+(pos*gridSize)*directionY)*dpi
+            )
+          })
+          ctx.closePath()
+          ctx.fill()
+        }
+        function drawLayers(i,color1,color2){
+          setFillStyle(color1,ctx)
+          ctx.strokeStyle=color1;
+          ctx.lineWidth=4
+          ctx.lineJoin='round'
+          drawLayer(mountains.left[i],-1,-1);
+          drawLayer(mountains.right[i],1,-1);
+          setFillStyle(color2,ctx);
+          ctx.strokeStyle=color2;
+          drawLayer(mountains.left[i],-1,1);
+          drawLayer(mountains.right[i],1,1);
+          ctx.strokeStyle='transparent';
+        }
+        drawLayers(2,colors.pink,colors.green);
+        drawLayers(1,colors.purple,colors.pink);
+        drawLayers(0,colors.blackish,colors.purple);
+        return textureCanvas;
+      }
+      return {
+        stop:function(){
+          stopAnim=true;
+        }
+      }
     })
-    const createMountains = () => {
-      let textureCanvas=createCanvas(bounds.width, bounds.height)
-      sizeToBounds(bounds, dpi, textureCanvas)
-      let ctx=getContext(textureCanvas)
-      let factor=20
-      if(win.innerWidth<700) factor=15
-      let cols=Math.round(bounds.width/factor)
-      let gridSize=bounds.width/cols
-      let mountains={
-        left:[
-          '......kkkkjjjkkjjkjkkjjkkkkkjjjjjj..kkjj',
-          '....kkkjjj..kkkkjkkjjjj.......kkkkjjjjj',
-          '...kkjj...kkkkjjj..kkkjkjjjj.....kkkkjjkjjj',
-        ],
-        right:[
-          '......kkkkjjjkkjjjkkkjjkkkkkjjjjjkjj..kkkjjj',
-          '....kkkjjjkkkkjjjkkjj.......kkkkjjjjj',
-          '...kkjj.kkkkjjkkjjjjkkkkjkjjjj...kkkkjkjjjj',
-        ]
-      }
-      const drawLayer = (layer, directionX, directionY) => {
-        ctx.beginPath()
-        let pos=0
-        let started=false
-        repeat(layer.length, (i) => {
-          let dir=layer.charAt(i)
-          pos+=dir=='.'?0:(dir=='j'?-1:1)
-          if(pos!=0 && !started){
-            started=true
-            ctx.moveTo((middle.x+((i-1)*gridSize*directionX))*dpi,middle.y*dpi);
-          }
-          cts.lineTo(
-            (middle.x+(i*gridSize)*directionX)*dpi,
-            (middle.y+(pos*gridSize)*directionY)*dpi
-          )
-        })
-        ctx.closePath()
-        ctx.fill()
-      }
-      const drawLayers = (i,color1,color2) => {
-        setFillStyle(color1,ctx)
-        ctx.strokeStyle=color1;
-        ctx.lineWidth=4
-        ctx.lineJoin='round'
-        drawLayer(mountains.left[i],-1,-1);
-        drawLayer(mountains.right[i],1,-1);
-        setFillStyle(color2,ctx);
-        ctx.strokeStyle=color2;
-        drawLayer(mountains.left[i],-1,1);
-        drawLayer(mountains.right[i],1,1);
-        ctx.strokeStyle='transparent';
-      }
-      drawLayers(2,colors.pink,colors.green);
-      drawLayers(1,colors.purple,colors.pink);
-      drawLayers(0,colors.blackish,colors.purple);
-      return textureCanvas;
-    }
+    
   }
 
   (function(){
