3c19b4f2c4d2d589d176fd284f561c7deb5197de
Colin Sygiel <ctsygiel@gmail.com>
2019-03-04 21:14:08 -0800

    reformatted index

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-let express = require('express');
-let reload = require('reload')
+let express = require("express");
 let app = express();
+var path = require("path");
 
-app.use(express.static(__dirname + '/public'));
- 
+app.use(express.static(__dirname + "/public"));
+
 // set up the template engine
-app.set('views', './views');
-app.set('view engine', 'pug');
- 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 // GET response for '/'
-app.get('/', function (req, res) {
-    // render the 'index' template, and pass in a few variables
-    res.render('index', {
-        randomNumber: () => {
-            return Math.random() * 10
-        }
-    });
+app.get("/", function(req, res) {
+  // render the 'index' template, and pass in a few variables
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-reload(app);
- 
 // start up the server
-app.listen(3000, function () {
-    console.log('Listening on http://localhost:3000');
+app.listen(3000, function() {
+  console.log("Listening on http://localhost:3000");
 });
